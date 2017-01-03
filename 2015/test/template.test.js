432dd0559c9c4a16305b9379c4b11f49edef223b
Samuel Plumppu <greenheartmusic@gmail.com>
2017-01-04 00:22:06 +0100

    Prevent test template from failing on `npm test`.

--- 2015/test/template.test.js
+++ 2015/test/template.test.js
@@ -1,13 +1,13 @@
-const test = require('tape')
-// Replace all N with day
-test('Day N.A - ', t => {
-  const dayN = require('../solutions/N')
-  t.equal(dayN.a(), 'a')
-  t.end()
-})
-
-test('Day N.B - ', t => {
-  const dayN = require('../solutions/N')
-  t.equal(dayN.b(), 'b')
-  t.end()
-})
+// const test = require('tape')
+// // Replace all N with day
+// test('Day N.A - ', t => {
+//   const dayN = require('../solutions/N')
+//   t.equal(dayN.a(), 'a')
+//   t.end()
+// })
+//
+// test('Day N.B - ', t => {
+//   const dayN = require('../solutions/N')
+//   t.equal(dayN.b(), 'b')
+//   t.end()
+// })
